063fd160dcc3c6b86a72689871d36c07d42ab03f
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2024-01-08 13:14:03 +0600

    Create POST.js

---
+++ POST.js
@@ -0,0 +1,23 @@
+app.post("/api/users", async (req, res) => {
+  const body = req.body;
+  if (
+    !body ||
+    !body.firstName ||
+    !body.lastName ||
+    !body.email ||
+    !body.gender ||
+    !body.jobTitle
+  ) {
+    return res.status(400).json({
+      message: "All feild are required",
+    });
+  }
+   await User.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.jobTitle,
+  });
+  return res.status(201).json({ message: `result` });
+});
