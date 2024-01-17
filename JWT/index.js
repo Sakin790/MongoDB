d8ce4ae652d518a87d1804c8d4c84ddbc055fc9b
Sakin790 <sakinislam79@gmail.com>
2024-01-17 11:07:15 +0600

    SomeThing Wrong

--- JWT/index.js
+++ JWT/index.js
@@ -1,9 +1,16 @@
-import express from "express";
-const app = express();
-const PORT = 8080;
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running at port : ${PORT}`);
+import dotenv from "dotenv";
+import ConnectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({
+  path: "./env",
 });
+
+ConnectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server in running at PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connecion Error!!!", "from root index file", err);
+  });
